877d9b40f56a282465111e085031159fd9447475
Pierre <pierre@hubert.fr>
2018-05-09 18:37:44 +0200

    Updated Docker build configuration

--- system/config/docker.config.php
+++ system/config/docker.config.php
@@ -5,7 +5,10 @@
  * @author Pierre HUBERT
  */
 
-class Docker {
+//This configuration is based on the offline configuration
+require_once __DIR__."/offline.config.php";
+
+class Docker extends Offline {
 
 	/**
 	 * Container URL
@@ -30,52 +33,7 @@ class Docker {
 	const PROD_MODE = TRUE;
 
 	/**
-	 * Path to assets (relative to the build folder)
-	 */
-	const PATH_ASSETS = "assets/";
-
-	/**
 	 * Path to assets (URL)
 	 */
 	const ASSETS_URL = self::CONTAINER_URL."assets/";
-
-	/**
-	 * Third party CSS files
-	 */
-	const THIRD_PARTY_CSS = "third_party_css.css";
-
-	/**
-	 * Third party Javascript files
-	 */
-	const THIRD_PARTY_JS = "third_party.js";
-
-	/**
-	 * Third party Javascript files (unminified)
-	 */
-	const THIRD_PARTY_UNMINIFIED_JS = "third_party.unminified.js";
-
-	/**
-	 * Application CSS files
-	 */
-	const APP_CSS = "app.css";
-
-	/**
-	 * Application JS files
-	 */
-	const APP_JS = "app.js";
-
-	/**
-	 * Application JS files (unminifieds)
-	 */
-	const APP_UNMINIFIED_JS = "app.unminified.js";
-
-	/**
-	 * Language settings
-	 */
-	const DEFAULT_LANGUAGE = "en";
-
-	/**
-	 * Templates settings
-	 */
-	const TEMPLATES_PATH = "templates/";
 }
