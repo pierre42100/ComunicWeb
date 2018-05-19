d687e9962ea39aa7de6fee7fdbb9a3eb07e153fc
Pierre <pierre@hubert.fr>
2018-05-19 10:25:42 +0200

    Added Comunic.io build configuration

---
+++ system/config/comunic_io.config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * comunic.io build configuration
+ * 
+ * @author Pierre HUBERT
+ */
+
+//This configuration is based on the offline configuration
+require_once __DIR__."/offline.config.php";
+
+class Comunic_io extends Offline {
+
+	/**
+	 * API access and credentials
+	 */
+	const API_URL = "https://api.communiquons.org/";
+	const API_SERVICE_NAME = "ComunicIO";
+	const API_SERVICE_TOKEN = "UxvYud4xSOslVFANWYcD";
+
+	/**
+	 * Site URL
+	 */
+	const SITE_URL = "https://comunic.io/";
+
+	/**
+	 * Site production mode
+	 */
+	const PROD_MODE = TRUE;
+
+	/**
+	 * Path to assets (URL)
+	 */
+	const ASSETS_URL = "https://comunic.io/assets/";
+
+	/**
+	 * Site requires https connection
+	 */
+	const FORCE_HTTPS = true;
+
+}
