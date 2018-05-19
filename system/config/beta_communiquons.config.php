7abe97c1c24f2c9d0dba0f1fa761988124abbc54
Pierre <pierre@hubert.fr>
2018-05-19 10:22:24 +0200

    Updated beta configuration

--- system/config/beta_communiquons.config.php
+++ system/config/beta_communiquons.config.php
@@ -8,7 +8,7 @@
 //This configuration is based on the offline configuration
 require_once __DIR__."/offline.config.php";
 
-class Online extends Offline {
+class Beta_communiquons extends Offline {
 
 	/**
 	 * API access and credentials
@@ -31,5 +31,10 @@ class Online extends Offline {
 	 * Path to assets (URL)
 	 */
 	const ASSETS_URL = "https://beta.communiquons.org/assets/";
+
+	/**
+	 * Site requires https connection
+	 */
+	const FORCE_HTTPS = true;
 
 }
