2d88a71b80b27f8630b9da8c7f407691bd74302b
Pierre HUBERT <pierre.git@communiquons.org>
2019-01-21 08:30:56 +0100

    Fix issue

--- assets/zip/personnal-data-export-navigator-builder.sh
+++ assets/zip/personnal-data-export-navigator-builder.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $SCRIPT_DIR;
+
 rm -f personnal-data-export-navigator.zip
 cd personnal-data-export-navigator;
 zip -r personnal-data-export-navigator.zip assets Export.html
