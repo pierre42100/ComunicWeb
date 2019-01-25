99fffb839f66f0b68a6bcbd1435cc4c2d4547340
Pierre HUBERT <pierre.git@communiquons.org>
2019-01-25 19:03:23 +0100

    Each conversation use its own stream to media and audio stream

--- assets/js/components/calls/userMedia.js
+++ assets/js/components/calls/userMedia.js
@@ -7,22 +7,11 @@
 ComunicWeb.components.calls.userMedia = {
 
 	/**
-	 * Pointer on current user media
-	 */
-	_currMedia: undefined,
-
-	/**
 	 * Get user media
 	 * 
 	 * @return {Promise} A promise to get user media
 	 */
 	get: function(){
-
-		//Check if we have already got user media
-		if(this._currMedia != undefined && this._currMedia.active)
-			return new Promise(function(resolve, error){
-				resolve(ComunicWeb.components.calls.userMedia._currMedia);
-			});
 		
 		//Use latest API
 		return navigator.mediaDevices
