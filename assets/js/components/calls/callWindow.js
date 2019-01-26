0b806d5bb2cdc41a9e8c3c39a306171b6669f40c
Pierre HUBERT <pierre.git@communiquons.org>
2019-01-26 10:23:57 +0100

    Made call window draggable

--- assets/js/components/calls/callWindow.js
+++ assets/js/components/calls/callWindow.js
@@ -329,6 +329,50 @@ ComunicWeb.components.calls.callWindow = {
 		});
 
 
+
+		/**
+		 * Make the call window draggable
+		 */
+		{
+			var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+			call.window.title.onmousedown = function(e){
+				e = e || window.event;
+				e.preventDefault();
+
+				//get the mouse cursor position at startup
+				pos3 = e.clientX;
+				pos4 = e.clientY;
+				document.onmouseup = closeDragElement;
+				document.onmousemove = elementDrag;
+			}
+
+			function elementDrag(e){
+				e = e || window.event;
+				e.preventDefault();
+
+				//Calculate new cursor position
+				pos1 = pos3 - e.clientX;
+				pos2 = pos4 - e.clientY;
+				pos3 = e.clientX;
+				pos4 = e.clientY;
+
+				//Set element new position
+				callContainer.style.top = (callContainer.offsetTop - pos2) + "px";
+				callContainer.style.left = (callContainer.offsetLeft - pos1) + "px";
+			}
+
+			function closeDragElement(){
+
+				//Stop moving when mouse button is released
+				document.onmouseup = null;
+				document.onmousemove = null;
+			}
+		}
+
+
+
+
 		//Load user media
 		call.setLoadingMessage("Waiting for your microphone and camera...");
 
