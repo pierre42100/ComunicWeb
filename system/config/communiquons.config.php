fa923d33e8b0a269aa7bbce6c3df4c9a7031cc44
Pierre HUBERT <pierre.git@communiquons.org>
2019-12-24 11:03:26 +0100

    Add communiquons.org build config

---
+++ system/config/communiquons.config.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * comunic.io build configuration
+ * 
+ * @author Pierre HUBERT
+ */
+
+//This configuration is based on the offline configuration
+require_once __DIR__."/offline.config.php";
+
+class Communiquons extends Offline {
+
+	/**
+	 * API access and credentials
+	 */
+	const API_URL = "https://api.communiquons.org/";
+	const API_SERVICE_NAME = "ComunicWEB";
+	const API_SERVICE_TOKEN = "BVuWUt4m";
+
+	/**
+	 * Site URL
+	 */
+	const SITE_URL = "https://communiquons.org/";
+
+	/**
+	 * About website access
+	 */
+	const ABOUT_WEBSITE_URL = "https://about.communiquons.org/";
+
+	/**
+	 * Site production mode
+	 */
+	const PROD_MODE = TRUE;
+
+	/**
+	 * Path to assets (URL)
+	 */
+	const ASSETS_URL = "https://communiquons.org/assets/";
+
+	/**
+	 * Site requires https connection
+	 */
+	const FORCE_HTTPS = true;
+
+}
